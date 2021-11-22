1c6b9b1e5ebe1f228b6a3f69313454a9b8f5ba89
DavidHermi <91197896+DavidHermi@users.noreply.github.com>
2021-11-22 09:07:39 +0100

    Add files via upload

---
+++ Ejemplo_Switch/src/ejemplo_switch/Ejemplo_Switch.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_switch;
+
+/**
+ *
+ * @author dam1
+ */
+public class Ejemplo_Switch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
